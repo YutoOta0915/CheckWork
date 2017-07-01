9df3eb0d3c23593b2a031e58556535f7f0582683
大田　雄翔 <yutooota0915@icloud.com>
2017-07-01 15:54:39 +0900

    -つけました

--- CheckWorkapp/ViewController.swift
+++ CheckWorkapp/ViewController.swift
@@ -9,11 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label : UILabel!
+      var number : Int = 0
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func buuton()  {
+      
+        number = number + 1
+        label.text = String(number)
+    
+    }
+    
+    @IBAction func button2() {
+        
+        number = number - 1
+        label.text = String(number)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
